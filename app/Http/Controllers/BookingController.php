e36d01cad264ab53d5e0a28f4ea392c0765ccfeb
piyushkarmacharya <piyushkarmacharya65@gmail.com>
2024-08-10 11:59:51 +0545

    check if booking other have booked at given time or not

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -7,18 +7,24 @@ use Illuminate\Http\Request;
 class BookingController extends Controller
 {
     public function createBooking(Request $req){
-        $amount=$req->payment_amount;
-        $rate=$req->rate;
-        $booking=Booking::create([
-            "member_id"=>$req->member_id,
-            "date"=>$req->date,
-            "time"=>$req->time,
-            "rate"=>$rate,
-            "payment_amount"=>$amount,
-            "payment_cleared"=>$amount>=$rate,
-            // "approved"=>false its by default false
-        ]);
-       
-        return response()->json(['booked'=>true,'booking_details'=>$booking]);
+        $already_Booked=Booking::where('date',$req->date)->where('time',$req->time)->exists();
+        if($already_Booked){
+            return response()->json(['booked'=>false,'message'=>'Another team has already booked at that time']);
+        }else{
+            $amount=$req->payment_amount;
+            $rate=$req->rate;
+            $booking=Booking::create([
+                "member_id"=>$req->member_id,
+                "date"=>$req->date,
+                "time"=>$req->time,
+                "rate"=>$rate,
+                "payment_amount"=>$amount,
+                "payment_cleared"=>$amount>=$rate,
+                // "approved"=>false its by default false
+            ]);
+           
+            return response()->json(['booked'=>true,'booking_details'=>$booking]);
+        }
+        
     }
 }
